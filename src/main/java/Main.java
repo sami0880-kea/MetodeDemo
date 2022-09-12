474c3a8c66e5787e0f6deec5eca4a903ae331997
sami0880-kea <sami0880@stud.kea.dk>
2022-09-12 09:18:18 +0200

    Tilføjet Person konstruktør

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person(1.72);
+        Person p2 = new Person();
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    private double højde;
+
+    public Person(double højde) {
+        this.højde = højde;
+    }
+
+    public Person() {
+
+    }
+}
