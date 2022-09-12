eee5fcb0964a55b73bb1f6192cbdb3945624cd91
sami0880-kea <sami0880@stud.kea.dk>
2022-09-12 10:10:54 +0200

    Tilføjet get og set metode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Person p1 = new Person(1.74);
         Person p2 = new Person();
+        System.out.println(p1.getHøjde());
+
+        p1.setHøjde(1.76);
+        System.out.println("Ny højde: " + p1.getHøjde());
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,12 +1,23 @@
 public class Person {
 
     private double højde;
+    private int alder;
 
     public Person(double højde) {
         this.højde = højde;
     }
 
-    public Person() {
+    public Person() {}
+
+    public double getHøjde() {
+        return højde;
+    }
+
+    public void setHøjde(double højde) {
+        this.højde = højde;
+    }
+
+    public void harFødselsdag() {
 
     }
 }
