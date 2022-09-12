e497b289ab22eafbc69bd7be5c5a3b7f0e5cb1a2
sami0880-kea <sami0880@stud.kea.dk>
2022-09-12 09:34:15 +0200

    Opdateret højde

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Person p1 = new Person(1.72);
+        Person p1 = new Person(1.74);
         Person p2 = new Person();
     }
 }
